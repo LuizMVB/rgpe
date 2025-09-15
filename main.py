1da34b1670aa2bafdd6623c0f7949ac2dcc00153
gddeazevedo <gabrielcapuerj@gmail.com>
2025-09-15 19:55:47 -0300

    adicionando dataset o shank

--- main.py
+++ main.py
@@ -1,23 +1,11 @@
 import sys
-from rgpe.services.handle_demo_service import HandleDemoService
 
 
 def main():
     if len(sys.argv) < 2:
-        print("Usage: python main.py <demo_key>")
+        print("Error")
         exit(1)
 
-    demo_key = sys.argv[1]
-
-    if demo_key == 'demos':
-        demos = HandleDemoService.list_demos()
-        print("Demos:")
-        for demo in demos:
-            print(f"  {demo}")
-        exit(0)
-
-    HandleDemoService.run(demo_key)
-    exit(0)
 
 
 if __name__ == "__main__":
